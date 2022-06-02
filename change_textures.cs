60c536b2660142773c888c90fb5bd01d1bb0a59c
MarcoChavarin97 <104690790+MarcoChavarin97@users.noreply.github.com>
2022-06-02 18:57:32 -0500

    Add files via upload

---
+++ change_textures.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class change_textures : MonoBehaviour
+{
+    public Material[] materiales; //creamos una lista de materiales
+    public GameObject caraACambiar;
+
+    private Renderer dadoTextura;
+
+    void Start()
+    {
+        dadoTextura = caraACambiar.GetComponent<Renderer>();
+        dadoTextura.enabled = true;
+        dadoTextura.sharedMaterial = materiales[0]; //llama al primer material de la lista
+    }
+    
+    public void ChangeCubeTexture()
+    {
+        dadoTextura.sharedMaterial = materiales[1];
+        //Este se llama desde unity, para cambiar el material al segundo en la lista
+    }
+}
